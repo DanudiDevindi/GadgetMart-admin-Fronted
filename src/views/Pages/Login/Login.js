efc4dc121416fe0b0a6e6095085967a9fa366f9e
Danudi <danusilva1997@gmail.com>
2022-02-01 11:39:56 +0530

    login page

---
+++ src/views/Pages/Login/Login.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import axios from '../../../axios';
+import {message} from 'antd';
+import 'antd/dist/antd.css';
+import '../../../css/custom.css';
+import Cookie from 'js-cookie';
+
+import {
+    Button,
+    Card,
+    CardBody,
+    CardGroup,
+    Col,
+    Container,
+    Form,
+    Input,
+    InputGroup,
+    InputGroupAddon,
+    InputGroupText,
+    Row
+  } 
+  from 'reactstrap';
+import img from '../../../assets/img/brand/gadget_mart.png';
+import {HOME_PATH} from "../../../routes";
+
+
---
+++ src/views/Pages/index.js
@@ -0,0 +1,13 @@
+import Login from './Login';
+import View from './View';
+import Add from './Add';
+import Home from './Home';
+import ManageProject from './ManageProject';
+import UserProfile from './UserProfile';
+import ChangePassword from './ChangePassword'
+import Configuration from './Configuration'
+import Report from './Report'
+
+export {
+  Login, View, Add, UserProfile, ManageProject, Home, ChangePassword, Configuration, Report
+};
