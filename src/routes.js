20e5f81c0aeca21375d1525ce26fd7fd2cc96e9e
Danudi <danusilva1997@gmail.com>
2022-02-02 12:36:05 +0530

    home page routers

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import DefaultLayout from './containers/DefaultLayout';
 
+
+const Home = React.lazy(() => import('./views/Pages/Home'));
 export const HOME_PATH = '/admin'
 
 const routes = [
 
     { path: HOME_PATH+'/', exact: true, name: '', component: DefaultLayout },
-    
+    { path: HOME_PATH+'/home', name: 'Dashboard', component: Home },
 
 ];
 
---
+++ src/views/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Cookies from "js-cookie";
+import {ButtonGroup, Card, CardBody, Col, Row,} from "reactstrap";
+import '../../../css/custom.css';
+import 'antd/dist/antd.css';
+import axios from "../../../axios-orders";
+import {HOME_PATH} from "../../../routes";
+
+class Home extends Component {
+
+}
+
+export default Home;
