5abb3f6b04cdedf69ee01cd36bc331d5ee3c5035
Danudi <danusilva1997@gmail.com>
2022-02-03 10:46:18 +0530

    provider routes

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import DefaultLayout from './containers/DefaultLayout';
 
 
 const Home = React.lazy(() => import('./views/Pages/Home'));
+const Providers = React.lazy(() => import('./views/Pages/Providers/Providers'));
 export const HOME_PATH = '/admin'
 
 const routes = [
 
     { path: HOME_PATH+'/', exact: true, name: '', component: DefaultLayout },
     { path: HOME_PATH+'/home', name: 'Dashboard', component: Home },
+    { path: HOME_PATH+'/service-providers', name: 'Service Providers', component: Providers },
 
 ];
 
---
+++ src/views/Pages/Providers/Providers.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Cookies from "js-cookie";
+import 'semantic-ui-css/semantic.min.css'
+import {Table} from "antd";
+import 'antd/dist/antd.css';
+import '../../../css/custom.css';
+import axios from "../../../axios-orders";
+import {Button} from "semantic-ui-react";
+import Swa2 from "sweetalert2";
+import {AiOutlineCloseCircle} from "react-icons/ai";
+
+
+class UserProfile extends Component {
+}
+
+export default UserProfile;
