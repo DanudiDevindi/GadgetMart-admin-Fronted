73b3e92fe1ae13bc24e45110bc18977a391c750b
Danudi <danusilva1997@gmail.com>
2022-02-03 11:19:54 +0530

    provider ui

--- src/views/Pages/Providers/Providers.js
+++ src/views/Pages/Providers/Providers.js
@@ -11,6 +11,74 @@ import {AiOutlineCloseCircle} from "react-icons/ai";
 
 
 class UserProfile extends Component {
+
+    render() {
+        return (
+          <div className="animated fadeIn">
+            <div>
+    
+              <div className="ui form">
+                <div className="inline field">
+                  <div className="fields">
+                    <div className="six wide field">
+                      <input
+                        value={this.state.providerName}
+                        onChange={(e) => {
+                          this.onChange(e)
+                        }}
+                        name="providerName"
+                        type="text"
+                        placeholder="Service Provider Name"/>
+                    </div>
+                    <div className="six wide field">
+                      <input
+                        value={this.state.providerUrl}
+                        onChange={(e) => {
+                          this.onChange(e)
+                        }}
+                        name="providerUrl"
+                        type="text"
+                        placeholder="Web Service Url"/>
+                    </div>
+                    <br/>
+                    <div className="four wide field">
+                      {
+                        !this.state.save &&
+                        <Button size='mini' className="positive ui button tableBtn bg-danger" onClick={() => {
+                          this.setState({
+                            save: true,
+                            providerId: '',
+                            providerName: '',
+                            providerUrl: '',
+                          })
+                        }}><AiOutlineCloseCircle size={20}/></Button>
+                      }
+    
+                      <Button
+                        className='mr-3 m-1 tableBtn'
+                        positive size='small'
+                        style={{'float': 'right', color: 'white'}}
+                        disabled={this.state.providerName === '' || this.state.providerUrl === ''}
+                        onClick={() => this.saveProvider(this.state.save ? 'post' : 'patch','')}
+                      >{this.state.save ? 'Add Service Provider' : 'Update Provider'}</Button>
+    
+                    </div>
+                  </div>
+                </div>
+              </div>
+    
+              <br/>
+              <Table
+                bordered={true}
+                pagination={{pageSize: 10}}
+                className=" text-center"
+                columns={this.createUserTableColumns}
+                dataSource={this.state.tableRow}
+              />
+            </div>
+          </div>
+        );
+      }
 }
 
 export default UserProfile;
