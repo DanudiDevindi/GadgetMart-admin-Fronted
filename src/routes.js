6a0f07e1bbb6fced9c34d23ec26d8846a470ea6f
Danudi <danusilva1997@gmail.com>
2022-02-04 11:28:58 +0530

    user routes

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import DefaultLayout from './containers/DefaultLayout';
 
 
 const Home = React.lazy(() => import('./views/Pages/Home'));
+const UserProfile = React.lazy(() => import('./views/Pages/UserProfile/UserProfile'));
 const Providers = React.lazy(() => import('./views/Pages/Providers/Providers'));
 export const HOME_PATH = '/admin'
 
@@ -10,6 +11,7 @@ const routes = [
 
     { path: HOME_PATH+'/', exact: true, name: '', component: DefaultLayout },
     { path: HOME_PATH+'/home', name: 'Dashboard', component: Home },
+    { path: HOME_PATH+'/user-profile', name: 'Registered Users', component: UserProfile },
     { path: HOME_PATH+'/service-providers', name: 'Service Providers', component: Providers },
 
 ];
---
+++ src/views/Pages/UserProfile/UserProfile.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Cookies from "js-cookie";
+import {Button, Dropdown} from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+import {notification, Switch, Table} from "antd";
+import 'antd/dist/antd.css';
+import '../../../css/custom.css';
+import {AiOutlineCheckCircle} from "react-icons/ai";
+import {MDBModal, MDBModalBody, MDBModalFooter, MDBModalHeader} from "mdbreact";
+import axios from "../../../axios-orders";
+import {Form, FormGroup, Input, InputGroup, InputGroupAddon, InputGroupText} from "reactstrap";
+import Swa2 from "sweetalert2";
+import {message} from "antd/lib/index";
+
+class UserProfile extends Component {
+}
+
+export default UserProfile;
